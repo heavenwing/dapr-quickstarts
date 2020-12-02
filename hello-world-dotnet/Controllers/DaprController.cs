f1256f68aad2e66ec6dc6fab390040ffd6c161bf
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2020-12-02 18:16:46 +0800

    add neworder into DaprController

--- hello-world-dotnet/Controllers/DaprController.cs
+++ hello-world-dotnet/Controllers/DaprController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -33,19 +36,63 @@ namespace hello_world_dotnet.Controllers
         {
             using var httpClient = _clientFactory.CreateClient();
             var request = new HttpRequestMessage(HttpMethod.Get, $"{stateUrl}/order");
-            var response = await httpClient.SendAsync(request);
+            HttpResponseMessage response;
+            try
+            {
+                response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                await Response.WriteAsync(ex.Message);
+                return new StatusCodeResult(500);
+            }
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                return NotFound(response.ReasonPhrase);
+
             var content = await response.Content?.ReadAsStringAsync();
             Debug.WriteLine($"Respone content: {content}");
-            if (!response.IsSuccessStatusCode
-                || string.IsNullOrWhiteSpace(content))
-            {
-                return NotFound(content);
-            }
-            
-            var orders = await response.Content.ReadFromJsonAsync<List<Order>>();
+            var orders = JsonSerializer.Deserialize<List<Order>>(content);
             if (orders?.Count > 0)
                 return Ok(orders);
             return NotFound();
+        }
+
+        [HttpPost("/neworder")]
+        public async Task<IActionResult> Post([FromBody] Order data)
+        {
+            Debug.WriteLine("Got a new order! Order ID: " + data.Id);
+
+            var state = new List<object>
+            {
+                new
+                {
+                    key="order",
+                    value=data
+                }
+            };
+
+            using var httpClient = _clientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, $"{stateUrl}");
+            request.Content = new StringContent(JsonSerializer.Serialize(state));
+            HttpResponseMessage response;
+            try
+            {
+                response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                await Response.WriteAsync(ex.Message);
+                return new StatusCodeResult(500);
+            }
+
+            if (!response.IsSuccessStatusCode)
+                return BadRequest(response.ReasonPhrase);
+
+            Debug.WriteLine("Successfully persisted state.");
+            return Ok();
         }
     }
 }
--- hello-world-dotnet/README.md
+++ hello-world-dotnet/README.md
@@ -1,3 +1,7 @@
 ﻿```sh
 dapr run --app-id dotnetapp --app-port 5001 --dapr-http-port 13500 -- dotnet run
 ```
+
+```powershell
+dapr invoke --app-id dotnetapp --method neworder --payload '{\"data\": { \"id\": \"42\" } }'
+```
